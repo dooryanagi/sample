f9cea5004aace5afd81fcc47c44ee64c30c5a320
EC2 Default User <ec2-user@ip-172-31-2-30.ap-northeast-1.compute.internal>
2024-05-09 08:11:08 +0000

    [Finish]3-11

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -13,10 +13,15 @@ class ListsController < ApplicationController
     # （4）トップ画面へリダイレクト→詳細画面へリダイレクト
     # redirect_to list_path(list.id)
 
+    # (1)データを受け取り新規登録するためのインスタンス作成
     @list = List.new(list_params)
+    #（2）データをデータベースに保存するためのsaveメソッド実行
     if @list.save
+      # （3）フラッシュメッセージを定義、詳細画面へリダイレクト
+      flash[:notice] = "投稿に成功しました"
       redirect_to list_path(@list.id)
     else
+      flash.now[:alert] = "投稿に失敗しました"
       render :new
     end
   end
